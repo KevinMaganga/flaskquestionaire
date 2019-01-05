6c33ef0ba2b3641645c518fea4867613b7c2d495
Kevin Maganga <Kevinmg700@gmail.com>
2019-01-05 10:14:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ import os
 
 #string =os.path.dirname(os.path.abspath(__file__))
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
-db = SQLAlchemy(app)
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+#db = SQLAlchemy(app)
 
 
 
